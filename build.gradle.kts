e774e58bdc8d2f86df18436499d3ff5dab3004cd
Mose <km_413@naver.com>
2023-04-24 15:54:16 +0900

    1. Add Ktor Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,26 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    application
     kotlin("jvm") version "1.6.21"
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    val ktor_version = "2.3.0"
+    val logback_version = "1.2.3"
     testImplementation(kotlin("test"))
+
+    // 1. Add Ktor dependencies
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-cio:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
 }
 
 tasks.test {
